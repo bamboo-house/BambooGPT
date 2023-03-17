4e53ce80f7ea3c20ecd577fbcf79666bcaa9fcb6
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-03-18 00:27:59 +0900

    openai導入

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,15 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   name: string;
 };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users/");
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users/');
   const users = await response.json();
-  res.status(200).json({ users });
+  res.status(200).json({ name: users[0].name });
 }
