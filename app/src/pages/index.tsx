7a3ab0bffd90a7b8abe5aaa2fb575ecd15fd3fe9
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-04-05 20:29:57 +0900

    初期化

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -3,6 +3,22 @@ import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 import { useState } from 'react';
 import { Form } from '@/components/Form';
+import { FirebaseOptions, initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_AMESUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default function Home() {
   const [result, setResult] = useState('');
