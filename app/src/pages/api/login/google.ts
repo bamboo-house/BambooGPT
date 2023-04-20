404f64bb0b36df6ff32b7ffbede3f726f5c51e9c
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-04-20 10:56:02 +0900

    例外は早く返す

--- app/src/pages/api/login/google.ts
+++ app/src/pages/api/login/google.ts
@@ -7,27 +7,27 @@ import { ReqLoginGoogle, ResLoginGoogle } from '@/bff/types/login';
 // あと、loginは割と特殊な処理なので、api/login/配下にまとめておきたい
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   
-  if (req.method === 'POST') {
-    // リクエストボディの取得
-    const reqBody: ReqLoginGoogle = req.body;
-    const googleUserInfo: GoogleUserInfo = reqBody.googleUserInfo;
-
-    try {
-      const loginService = new LoginService();
-      const user = await loginService.loginWithGoogle(googleUserInfo);
-
-      const resBody: ResLoginGoogle = { 
-        body: {
-          name: user.name || '',
-          image: user.image || '',
-        }
-      };
-
-      res.status(200).json(resBody);
-    } catch (e) {
-      res.status(500).json({ error: { message: e.message } });
-    }
-  } else {
+  if (req.method !== 'POST') {
     res.status(400).json({ error: { message: '無効なリクエストです' } });
+  }
+  
+  // リクエストボディの取得
+  const reqBody: ReqLoginGoogle = req.body;
+  const googleUserInfo: GoogleUserInfo = reqBody.googleUserInfo;
+
+  try {
+    const loginService = new LoginService();
+    const user = await loginService.loginWithGoogle(googleUserInfo);
+
+    const resBody: ResLoginGoogle = { 
+      body: {
+        name: user.name || '',
+        image: user.image || '',
+      }
+    };
+
+    res.status(200).json(resBody);
+  } catch (e) {
+    res.status(500).json({ error: { message: e.message } });
   }
 }
