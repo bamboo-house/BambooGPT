6baac46bbe73d8f9d36e90d988e9532b3b21e2bc
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-06-01 14:57:16 +0900

    全体レイアウト調整

--- app/src/frontend/components/Top.tsx
+++ app/src/frontend/components/Top.tsx
@@ -3,38 +3,49 @@ import { useState } from 'react';
 export const Top = () => {
   const [showRightSidebar, setShowRightSidebar] = useState(false);
   return (
-    <div className="top-main flex h-screen w-full overflow-hidden">
-      <div className="top-leftsidebar w-[260px] flex-none bg-gpt-dark">
-        <div>LeftSidebar</div>
+    <div className="top-main flex h-screen w-full overflow-y-auto overflow-x-hidden">
+      <div className="top-leftsidebar h-full w-64 flex-none ">
+        <div className="fixed left-0 top-0 h-full w-64 bg-gpt-dark">LeftSidebar</div>
       </div>
 
       <div className="top-body w-full">
-        <div className="top-topbar flex h-8  items-center border border-gpt-dark border-b-zinc-500">
-          <p>oo</p>
-          <button
-            className="ml-auto"
-            onClick={() => {
-              setShowRightSidebar(!showRightSidebar);
-            }}
-          >
-            ボタン
-          </button>
+        <div className="top-topbar fixed left-64 right-0 top-0 h-8 border border-gpt-dark border-b-zinc-500 bg-gpt-dark">
+          <div className="flex items-center">
+            <p>oo</p>
+            <button
+              className="ml-auto"
+              onClick={() => {
+                setShowRightSidebar(!showRightSidebar);
+              }}
+            >
+              ボタン
+            </button>
+          </div>
         </div>
 
-        <div className="top-content relative flex h-full w-full">
-          <div className="top-chatlog flex">
-            <div className="h-32 w-32 bg-red-300">海運業</div>
-            <div className="h-32 w-32 bg-red-300">海運業</div>
-            <div className="h-32 w-32 bg-red-300">海運業</div>
-            <div className="h-32 w-32 bg-red-300">海運業</div>
-            <div className="h-32 w-32 bg-red-300">海運業</div>
-            <div className="h-32 w-32 bg-red-300">海運業</div>
-            <div className="h-32 w-32 bg-red-300">海運業</div>
-            <div className="h-32 w-32 bg-red-300">海運業</div>
-            <div className="h-32 w-32 bg-red-300">海運業</div>
+        <div className="top-content flex h-full w-full">
+          <div className="top-chatlog mt-8">
+            <div className="flex flex-wrap">
+              <div className="h-32 w-32 bg-red-300">
+                ===============================海運業ffffffff
+              </div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">
+                =======ゾウせんし==そらりん=海運業ffffffff海運業ffffffff 海運業ffffffff
+              </div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+            </div>
           </div>
           <div
-            className={`absolute right-0 top-0 h-full w-[260px] border border-gpt-dark border-l-zinc-500  ${
+            className={`fixed right-0 top-8 h-full w-64 border border-gpt-dark border-l-zinc-500 bg-gpt-gray  ${
               showRightSidebar
                 ? 'transition-transform duration-300 ease-in-out'
                 : 'translate-x-full transition-transform duration-300 ease-in-out'
