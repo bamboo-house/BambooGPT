d65c455433d42ee22c37144dff03daba1563802e
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-06-01 15:42:46 +0900

    バグ手性

--- app/src/frontend/components/Top.tsx
+++ app/src/frontend/components/Top.tsx
@@ -12,7 +12,7 @@ export const Top = () => {
       </div>
 
       <div className="top-body w-full">
-        <div className="fixed left-64 right-0 top-0 h-full">
+        <div className="fixed left-64 right-0 top-0">
           {/* 下記、TopBarコンポーネントにできる */}
           <div className="top-topbar h-8 border border-gpt-dark border-b-zinc-500 bg-gpt-gray">
             <div className="flex items-center">
@@ -30,67 +30,64 @@ export const Top = () => {
         </div>
 
         <div className="top-content flex h-full w-full">
-          <div className="mt-8">
-            {/* 下記、ChatLogコンポーネントにできる */}
-            <div className="top-chatlog flex flex-wrap">
-              <div className="h-32 w-32 bg-red-300">
-                ===============================海運業ffffffff
-              </div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">
-                =======ゾウせんし==そらりん=海運業ffffffff海運業ffffffff 海運業ffffffff
-              </div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">
-                =======ゾウせんし==そらりん=海運業ffffffff海運業ffffffff 海運業ffffffff
-              </div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">
-                =======ゾウせんし==そらりん=海運業ffffffff海運業ffffffff 海運業ffffffff
-              </div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">
-                =======ゾウせんし==そらりん=海運業ffffffff海運業ffffffff 海運業ffffffff
-              </div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
-              <div className="h-32 w-32 bg-red-300">海運業</div>
+          {/* 下記、ChatLogコンポーネントにできる */}
+          <div className="top-chatlog mt-8 flex flex-wrap">
+            <div className="h-32 w-32 bg-red-300">
+              ===============================海運業ffffffff
             </div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">
+              =======ゾウせんし==そらりん=海運業ffffffff海運業ffffffff 海運業ffffffff
+            </div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">
+              =======ゾウせんし==そらりん=海運業ffffffff海運業ffffffff 海運業ffffffff
+            </div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">
+              =======ゾウせんし==そらりん=海運業ffffffff海運業ffffffff 海運業ffffffff
+            </div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">
+              =======ゾウせんし==そらりん=海運業ffffffff海運業ffffffff 海運業ffffffff
+            </div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
+            <div className="h-32 w-32 bg-red-300">海運業</div>
           </div>
           {/* <div className="fixed bottom-10 bg-gpt-dark">メッセージフォーム</div> */}
-          <div className="fixed right-0 top-8 h-full">
-            {/* 下記、RigtSidebarコンポーネントにできる  */}
-            <div
-              className={`h-full w-64 border border-gpt-dark border-l-zinc-500 bg-gpt-gray  ${
-                showRightSidebar
-                  ? 'transition-transform duration-300 ease-in-out'
-                  : 'translate-x-full transition-transform duration-300 ease-in-out'
-              }`}
-            >
-              <div>RightSidebar</div>
-            </div>
+
+          {/* 下記、RigtSidebarコンポーネントにできる  */}
+          <div
+            className={`fixed right-0 top-8 h-full w-64 border border-gpt-dark border-l-zinc-500 bg-gpt-gray ${
+              showRightSidebar
+                ? 'transition-transform duration-300 ease-in-out'
+                : 'translate-x-full transition-transform duration-300 ease-in-out'
+            }`}
+          >
+            <div>RightSidebar</div>
           </div>
         </div>
       </div>
