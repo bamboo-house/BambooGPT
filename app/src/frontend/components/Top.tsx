0a084bed14b383475c7d45ae375150291963a206
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-06-04 00:54:11 +0900

    固まりつつある

--- app/src/frontend/components/Top.tsx
+++ app/src/frontend/components/Top.tsx
@@ -23,16 +23,19 @@ export const Top = () => {
     setShowRightSidebar(!showRightSidebar);
   };
 
+  // TODO: レイアウトコンポーネント、ロジックコンポーネント、ブロックコンポーネントに分ける
   return (
-    <div className="top-main flex h-screen w-full overflow-y-auto overflow-x-hidden">
+    <div className="top-main flex h-full w-full overflow-y-auto overflow-x-hidden">
+      {/* スマホの時,ヘッダーのボタンを押すと、w-64にすればサイドバーが開ける */}
+
       <div className="h-full w-64 flex-none md:w-0">
-        <div className="md:-0 fixed left-0 top-0 h-full">
+        <div className="fixed left-0 top-0 h-full">
           {/* 下記、LeftSidebarコンポーネントにできる */}
-          <div className="top-leftbar h-full w-64 bg-gpt-dark md:hidden">LeftSidebar</div>
+          <div className="top-leftbar h-full w-64 bg-gpt-dark">LeftSidebar</div>
         </div>
       </div>
 
-      <div className="top-body relative w-full">
+      <div className="top-body relative">
         {/* 下記、TopBarコンポーネントにできる */}
         <div className="top-topbar fixed top-0 z-10 h-10 w-full border border-gpt-dark border-b-zinc-500 bg-gpt-gray ">
           <div className="flex w-full items-center">
@@ -48,13 +51,23 @@ export const Top = () => {
           </div>
         </div>
 
-        <div className="top-content mt-8 flex h-full w-full">
+        <div className="top-content mt-10 flex ">
           {/* 下記、ChatLogコンポーネントにできる */}
           <div>
             <div className="top-chatlog flex flex-wrap">
               <div id="content" className="h-32 w-32 bg-red-300">
                 ===============================海運業ffffffff
               </div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
+              <div className="h-32 w-32 bg-red-300">海運業</div>
               <div className="h-32 w-32 bg-red-300">海運業</div>
               <div className="h-32 w-32 bg-red-300">海運業</div>
               <div className="h-32 w-32 bg-red-300">海運業</div>
@@ -104,7 +117,7 @@ export const Top = () => {
           {/* 下記、RigtSidebarコンポーネントにできる  */}
           <div
             id="right-sidebar"
-            className={`fixed right-0 top-8 h-full w-64 border border-gpt-dark border-l-zinc-500 bg-gpt-gray ${
+            className={`fixed right-0 top-10 h-full w-64 border border-gpt-dark border-l-zinc-500 bg-gpt-gray ${
               showRightSidebar
                 ? 'transition-transform duration-300 ease-in-out'
                 : 'translate-x-full transition-transform duration-300 ease-in-out'
