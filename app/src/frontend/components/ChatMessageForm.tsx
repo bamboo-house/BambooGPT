8e72fc2913845f8f2b8ca8a41e71cc9261b6e0fd
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-06-12 13:34:56 +0900

    formタグ使わない

--- app/src/frontend/components/ChatMessageForm.tsx
+++ app/src/frontend/components/ChatMessageForm.tsx
@@ -1,24 +1,32 @@
 import { KeyboardEvent } from 'react';
 
 export const ChatMessageForm = () => {
-  const handleTextareaKeydown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+  const handleTextareaKeydown = (e: any) => {
+    const target = e.target;
+    // もし、cmd + Enter なら、送信する
     if (e.key === 'Enter' && (e.ctrlKey || e.metaKey)) {
       console.log('key down cmd + Enter');
     }
-    if (e.key === 'Enter' || e.keyCode === 13) {
-      e.preventDefault();
+
+    // もし、Enterなら、heightを伸ばす
+    if (e.key === 'Enter' || e.code === 'Enter') {
       console.log('Enter keydown');
+    }
+
+    // もし、backspaceで、改行を消すなら、heightを短くする
+    if (e.key === 'Backspace' && target.value.substr(-1) === '\n') {
+      console.log('key down backspace');
     }
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     console.log('submit');
   };
 
   return (
     <div className="top-message-form border border-gpt-gray border-t-zinc-500 bg-gpt-gray">
-      <form className="mx-3 mb-16" onSubmit={handleSubmit}>
+      <div className="mx-3 mb-16">
         <div className="mx-auto my-2 flex w-full max-w-3xl flex-row">
           <textarea
             id="prompt-textarea"
@@ -27,11 +35,18 @@ export const ChatMessageForm = () => {
             onKeyDown={(e) => {
               handleTextareaKeydown(e);
             }}
-            className="m-0 h-40 max-h-52 w-full resize-none rounded-xl p-0 py-3 pl-3 pr-10 dark:bg-gpt-gray2"
+            className="m-0 max-h-52 w-full resize-none rounded-xl p-0 py-3 pl-3 pr-10 dark:bg-gpt-gray2"
           ></textarea>
         </div>
-        <input type="submit" />
-      </form>
+        <button
+          type="button"
+          onClick={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          送信
+        </button>
+      </div>
     </div>
   );
 };
