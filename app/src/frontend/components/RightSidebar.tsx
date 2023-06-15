a6d1bf092119b1751e05c8ed499f1e5721473eb1
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-06-16 00:57:39 +0900

    ロジック以外完成

--- app/src/frontend/components/RightSidebar.tsx
+++ app/src/frontend/components/RightSidebar.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import CreatableSelect from 'react-select/creatable';
 import { useRecoilState } from 'recoil';
 import { chatOptionState } from '../globalStates/atoms/chatAtom';
 
@@ -20,9 +21,43 @@ export const RightSidebar = ({ showRightSidebar }: { showRightSidebar: boolean }
     setChatOption({ ...chatOption, [name]: Number(value) });
   };
 
-  const handleChangeRnage = (e: any) => {
+  const handleChange = (e: any) => {
     e.preventDefault();
-    const { name, value } = e.target;
+    let { type, name, value } = e.target;
+    console.log(e.target);
+
+    // temperatureなら、0から2まで
+    // top_pなら、0から1まで
+    // stop
+    // presence_penaltyなら、0から2まで
+    // frequency_penaltyなら、0から2まで
+
+    // バリデーション
+    switch (name) {
+      case 'temperature':
+      case 'presence_penalty':
+      case 'frequency_penalty':
+        if (isNaN(Number(value))) {
+          value = 1;
+        } else if (value < 0) {
+          value = 0;
+        } else if (value > 2) {
+          value = 2;
+        }
+        break;
+      case 'top_p':
+        if (isNaN(Number(value))) {
+          value = 1;
+        } else if (value < 0) {
+          value = 0;
+        } else if (value > 1) {
+          value = 1;
+        }
+        break;
+      default:
+        break;
+    }
+
     // recoil公式ドキュメントのstata更新は、prevStateを使ってないのでここも同じようにしてみる
     // chatOptionForDisplayはtextを保存して、小数点も表示できるようにする
     setChatOptionForDisplay({ ...chatOptionForDisplay, [name]: value });
@@ -33,15 +68,15 @@ export const RightSidebar = ({ showRightSidebar }: { showRightSidebar: boolean }
     <div className={showClass}>
       <div className="fixed h-full w-[inherit] border border-gpt-dark border-l-zinc-500">
         <div className="mx-8">
-          <div className="my-6">
-            <div className="mb-2 flex items-center">
+          <div className="mb-12 mt-4">
+            <div className="flex items-center">
               <span className="flex-auto">Temprature</span>
               <input
                 type="text"
                 className="w-10 bg-transparent pr-1 text-right"
                 name="temperature"
                 value={chatOptionForDisplay.temperature}
-                onInput={handleChangeText}
+                onInput={handleChange}
               />
             </div>
 
@@ -53,19 +88,19 @@ export const RightSidebar = ({ showRightSidebar }: { showRightSidebar: boolean }
               value={chatOption.temperature}
               step={0.01}
               className="input-range-slider"
-              onInput={handleChangeRnage}
+              onInput={handleChange}
             />
           </div>
 
-          <div className="my-6">
-            <div className="mb-2 flex items-center">
+          <div className="mb-12 mt-4">
+            <div className="flex items-center">
               <span className="flex-auto">Top_p</span>
               <input
                 type="text"
                 className="w-10 bg-transparent pr-1 text-right"
                 name="top_p"
                 value={chatOptionForDisplay.top_p}
-                onInput={handleChangeText}
+                onInput={handleChange}
               />
             </div>
 
@@ -77,7 +112,105 @@ export const RightSidebar = ({ showRightSidebar }: { showRightSidebar: boolean }
               value={chatOption.top_p}
               step={0.01}
               className="input-range-slider"
-              onInput={handleChangeRnage}
+              onInput={handleChange}
+            />
+          </div>
+
+          <div className="mb-12 mt-4">
+            <div className=" items-center">
+              <span className="flex-auto">Top_p</span>
+            </div>
+            <CreatableSelect
+              isMulti
+              instanceId="selectbox"
+              theme={(theme) => ({
+                ...theme,
+                colors: {
+                  ...theme.colors,
+                  primary: 'none',
+                },
+              })}
+              styles={{
+                control: (baseStyles, state) => ({
+                  ...baseStyles,
+                  backgroundColor: 'transparent',
+                  // borderColor: state.isFocused ? 'rgb(156 163 175)' : 'gray',
+                }),
+                input: (baseStyles, state) => ({
+                  ...baseStyles,
+                  color: 'white',
+                }),
+                option: (baseStyles, state) => ({
+                  ...baseStyles,
+                  backgroundColor: 'transparent',
+                }),
+                menu: (baseStyles, state) => ({
+                  ...baseStyles,
+                  backgroundColor: 'transparent',
+                }),
+                multiValue: (baseStyles, state) => ({
+                  ...baseStyles,
+                  backgroundColor: '#666671',
+                }),
+                multiValueLabel: (baseStyles, state) => ({
+                  ...baseStyles,
+                  color: 'white',
+                }),
+                indicatorsContainer: (baseStyles, state) => ({
+                  display: 'none',
+                }),
+              }}
+              className="bg-gpt-gray2"
+              noOptionsMessage={() => 'Enter a sequence'}
+              placeholder=""
+            />
+          </div>
+
+          <div className="mb-12 mt-4">
+            <div className="flex items-center">
+              <span className="flex-auto">Frequency penalty</span>
+              <input
+                type="text"
+                className="w-10 bg-transparent pr-1 text-right"
+                name="frequency_penalty"
+                value={chatOptionForDisplay.frequency_penalty}
+                onInput={handleChange}
+              />
+            </div>
+
+            <input
+              type="range"
+              name="frequency_penalty"
+              max={2.0}
+              min={0.0}
+              value={chatOption.frequency_penalty}
+              step={0.01}
+              className="input-range-slider"
+              onInput={handleChange}
+            />
+          </div>
+
+          <div className="mb-12 mt-4">
+            <div className="flex items-center">
+              <span className="flex-auto">Presence penalty</span>
+              <input
+                type="text"
+                className="w-10 bg-transparent pr-1 text-right"
+                name="presence_penalty"
+                value={chatOptionForDisplay.presence_penalty}
+                onInput={handleChange}
+              />
+            </div>
+
+            <input
+              type="range"
+              name="presence_penalty"
+              max={2.0}
+              min={0.0}
+              value={chatOption.presence_penalty}
+              step={0.01}
+              className="input-range-slider"
+              onInput={handleChange}
             />
           </div>
         </div>
