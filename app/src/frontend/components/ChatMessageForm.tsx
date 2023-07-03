777f37deb517866fb4f824f985e480d5f94f42b2
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-07-03 22:25:21 +0900

    結果をstreamで取得するまで

--- app/src/frontend/components/ChatMessageForm.tsx
+++ app/src/frontend/components/ChatMessageForm.tsx
@@ -1,22 +1,119 @@
+import { getAuth } from 'firebase/auth';
+import { ChatCompletionRequestMessage, CreateChatCompletionRequest } from 'openai';
 import { useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { chatMessageListState } from '../globalStates/atoms/chatAtom';
+import { ReqPostOpenaiChat } from '@/bff/types/openai/chats';
 
 export const ChatMessageForm = () => {
+  const [chatMessageList, setChatMessageList] = useRecoilState(chatMessageListState);
+
   const [isReceiving, setIsReceiving] = useState(false);
+  const [prompt, setPrompt] = useState('');
+  const [result, setResult] = useState('');
 
   const handleTextareaKeydown = (e: any) => {
-    // もし、cmd + Enter なら送信する
-    if (e.key === 'Enter' && (e.ctrlKey || e.metaKey)) {
+    // 「cmd + Enter 」かつ「受信中でない」場合、送信する
+    if (e.key === 'Enter' && (e.ctrlKey || e.metaKey) && !isReceiving) {
       console.log('key down cmd + Enter');
-      if (isReceiving) {
-        handleSubmit(e);
-      }
+      handleSubmit();
     }
   };
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async () => {
     console.log('submit');
     setIsReceiving(true);
+
+    const newChatMessageList: ChatCompletionRequestMessage[] = [
+      ...chatMessageList,
+      { role: 'user', content: prompt },
+    ];
+    // promptをChatMessageListに追加する
+    setChatMessageList(newChatMessageList);
     // POST /api/openai/chatsにリクエストを送る
+    await openaiChats(newChatMessageList, (text: string) => setResult((prev) => prev + text));
+
+    // レスポンスをmessageListに追加する
+    console.log(prompt);
+  };
+
+  const openaiChats = async (
+    messages: ChatCompletionRequestMessage[],
+    resText: (text: string) => void
+  ) => {
+    const user = getAuth().currentUser;
+    if (!user) {
+      return;
+    }
+    const idToken = await user.getIdToken();
+
+    console.log('chatMessageList', chatMessageList);
+
+    const reqBody: ReqPostOpenaiChat = {
+      uid: user.uid,
+      threadId: 'QlunF5Ke2kXNF6Sq0agP',
+      chatContent: {
+        model: 'gpt-3.5-turbo',
+        messages: messages,
+        temperature: undefined,
+        top_p: undefined,
+        n: undefined,
+        stream: true,
+        stop: undefined,
+        max_tokens: undefined,
+        presence_penalty: undefined,
+        frequency_penalty: undefined,
+        logit_bias: undefined,
+        user: undefined,
+      },
+    };
+
+    const response = await fetch('/api/openai/chats', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${idToken}`,
+      },
+      body: JSON.stringify(reqBody),
+    });
+
+    if (!response.body) {
+      console.error('Network response was not ok');
+      throw new Error('Network response was not ok');
+    }
+
+    if (!response.ok) {
+      const data = await response.json();
+      throw new Error(data.error.message);
+    }
+
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder();
+
+    while (true) {
+      const { value, done } = await reader.read();
+      if (done) {
+        break;
+      }
+      try {
+        const dataString = decoder.decode(value);
+        let text: string;
+
+        // ここでdataStringが"{"text":"ダルビッシュ"}{"text":"影山"}"になっていた場合は、"ダルビッシュ影山"を保存する
+        const counter = dataString.match(/text/g)?.length;
+        if (counter && counter > 1) {
+          const str = '[' + dataString.replace(/"}{"/g, '"},{"') + ']';
+          const array = JSON.parse(str);
+          text = array.map((obj: { text: string }) => obj.text).join('');
+        } else {
+          text = JSON.parse(dataString).text;
+        }
+
+        resText(text);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   };
 
   const resizeTextarea = () => {
@@ -41,7 +138,8 @@ export const ChatMessageForm = () => {
 
   return (
     <div className="absolute inset-x-0 bottom-0 bg-gpt-linear-gradient pb-16">
-      <div className="relative mx-auto my-2 max-w-3xl rounded-xl py-3 pl-3 dark:bg-gpt-gray2 tb:mx-3">
+      {result}
+      <div className="relative mx-auto my-2 max-w-3xl rounded-xl pb-2 pl-3 pt-3 dark:bg-gpt-gray2 tb:mx-3">
         <textarea
           id="promptTextAreaId"
           rows={1}
@@ -50,15 +148,19 @@ export const ChatMessageForm = () => {
           onKeyDown={(e) => {
             handleTextareaKeydown(e);
           }}
+          value={prompt}
+          onChange={(e) => {
+            setPrompt(e.target.value);
+          }}
           onInput={resizeTextarea}
           className="m-0 max-h-52 w-full resize-none bg-transparent pr-12 focus:outline-none"
         ></textarea>
         <button
           type="button"
           style={{ backgroundColor: 'rgb(25, 195, 125)' }}
-          className="absolute bottom-3 right-4 rounded-md p-1"
+          className="absolute bottom-2 right-4 rounded-md p-1"
           onClick={(e) => {
-            handleSubmit(e);
+            handleSubmit();
           }}
           disabled={isReceiving}
         >
