dc6f089a38ae3612d97f21e0df317b84f4be3960
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-07-11 20:00:10 +0900

    svgからreact-iconに変更

--- app/src/frontend/components/LeftSidebar.tsx
+++ app/src/frontend/components/LeftSidebar.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Key, useEffect, useState } from 'react';
+import { BiComment } from 'react-icons/bi';
+import { IoSettingsOutline } from 'react-icons/io5';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { currentUserState } from '../globalStates/atoms/currentUserAtom';
 import { threadListState } from '../globalStates/atoms/threadAtom';
@@ -44,15 +46,26 @@ export const LeftSidebar = () => {
         </button>
 
         <Modal isOpen={isSettingsOpen} onClose={() => setIsSettingsOpen(false)}>
-          <div className="absolute left-60 top-[-40px] h-52 w-52 bg-red-500">unko</div>
+          <div className="absolute left-60 top-[-40px] w-52 rounded bg-black">
+            <ul className="p-3">
+              <li>
+                <Link href={''}>
+                  <IoSettingsOutline size={19} />
+                  Settings
+                </Link>
+              </li>
+              <li>
+                <button
+                  className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                  onClick={handleChangeLogout}
+                >
+                  ログアウト
+                </button>
+              </li>
+            </ul>
+          </div>
         </Modal>
 
-        <button
-          className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-          onClick={handleChangeLogout}
-        >
-          ログアウト
-        </button>
         <div className="h-4/5 flex-1 overflow-y-auto overflow-x-hidden">
           {data &&
             data.body &&
@@ -62,20 +75,7 @@ export const LeftSidebar = () => {
                 className="mx-2 flex cursor-pointer items-center gap-3 rounded-md p-3 hover:bg-gpt-gray"
                 key={key}
               >
-                <svg
-                  stroke="currentColor"
-                  fill="none"
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="h-4 w-4"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-                </svg>
+                <BiComment size={19} />
                 <div className="w-full overflow-hidden text-ellipsis whitespace-nowrap break-all">
                   {thread.name}
                 </div>
--- app/src/frontend/components/TopBar.tsx
+++ app/src/frontend/components/TopBar.tsx
@@ -1,3 +1,4 @@
+import { LuPanelLeft } from 'react-icons/lu';
 import { useRecoilState } from 'recoil';
 import { isOpenedRightSidebarState } from '../globalStates/atoms/isOpenedRightSidebarAtom';
 
@@ -11,28 +12,14 @@ export const TopBar = () => {
         <div className="w-64 flex-none">
           <div className="flex flex-row-reverse">
             <button
-              className={`mr-4 mt-[6px] rounded-[3px] px-1 py-[3px] ${
+              className={`mr-4 mt-[6px] rounded-[3px] px-1 py-[2px] ${
                 isOpenedRightSidebar ? 'bg-gray-500' : ''
               }`}
               onClick={() => {
                 setIsOpenedRightSidebar(!isOpenedRightSidebar);
               }}
             >
-              <svg
-                stroke="currentColor"
-                fill="none"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="h-5 w-5"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect x="1" y="3" width="22" height="18" rx="2" ry="2"></rect>
-                <line x1="9" y1="3" x2="9" y2="21"></line>
-              </svg>
+              <LuPanelLeft size={22} />
             </button>
           </div>
         </div>
